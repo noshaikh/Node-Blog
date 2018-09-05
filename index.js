1df2140f8aab2f484d6271edd065d268c8c9e48a
Naaz Shaikh <naazhome@Naazs-MacBook-Pro-3.local>
2018-09-05 13:02:14 -0700

    working on CRUD

---
+++ index.js
@@ -0,0 +1,108 @@
+const express = require("express");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const cors = require("cors");
+
+const db = require("./data/helpers/userDb.js");
+const server = express();
+server.use(express.json());
+
+server.get("/", (req, res) => {
+  res.send("Hello World");
+});
+// GET /api/users   for array/list of users
+server.get("/users", (req, res) => {
+  db.get()
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(err => {
+      console.error("error", err);
+      res
+        .status(500)
+        .json({ error: "The users information could not be retrieved" });
+    });
+});
+// GET /api/users/:id  for specific id of user
+//PUT /api/users/:id you have to put updated body in the request this is to update the specific id
+//DELETE /api/users/:id all you need to delete a customer is the id
+//POST /api/users we are posting a new customer to the list. we need to have the body in the request.
+
+server.get("/users/:id", (req, res) => {
+  db.get(req.params.id) // to access ids, refer react router stuff.
+    .then(user => {
+      if (user) {
+        res.status(200).json(user);
+      } else {
+        res
+          .status(404)
+          .json({ message: "The user with the specified ID does not exist." })
+
+          .catch(err => {
+            res
+              .status(500)
+              .json({ error: "The user information could not be retrieved." });
+          });
+      }
+    });
+});
+
+server.post("/users", (req, res) => {
+  const user = req.body;
+  if (user.name) {
+    try {
+      const response = db.insert(user);
+      res.status(201).json(user);
+    } catch (err) {
+      res.status(500).json({
+        error: "There was an error while saving the user to the database"
+      });
+    }
+  } else {
+    res.status(400).json({
+      errorMessage: "Please provide name for the user."
+    });
+  }
+});
+
+server.put("/users/:id", (req, res) => {
+  const { id } = req.params;
+  const user = req.body;
+  db.update(id, user).then(count => {
+    if (!count) {
+      res
+        .status(404)
+        .json({ message: "The user with the specified ID does not exist." });
+    } else if (post.title && post.contents) {
+      try {
+        db.update(id, user);
+        res.status(200).json(count);
+      } catch (err) {
+        res
+          .status(500)
+          .json({ message: "The post information could not be modified" });
+      }
+    } else {
+      res.status(400).json({
+        errorMessage: "Please provide title and contents for the post."
+      });
+    }
+  });
+});
+
+server.delete("/users/:id", (req, res) => {
+  const { id } = req.params;
+  db.remove(id)
+    .then(count => {
+      if (count) {
+        res.status(204).end();
+      } else {
+        res
+          .status(404)
+          .json({ message: "The post with the specified ID does not exist" });
+      }
+    })
+    .catch(err => res.status(500).json(err));
+});
+
+server.listen(9000, () => console.log("\n== API on port 9k ==\n"));
